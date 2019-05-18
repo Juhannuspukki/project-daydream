222570e0296aa0a82d02b812f212a113200f883b
Juhannuspukki <jere@jere.pro>
2019-05-18 21:04:51 +0300

    Update footer for courserobot era

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
   <Container className={"Tin-Can"}>
     <Row className={"Footer-Row"}>
       <Col md="4">
-        <a className={"Footer-Logo"} href='https://t.me/courserobot' target="_blank" rel="noopener noreferrer">??? ??? ???</a>
+        <a className={"Footer-Logo"} href='https://t.me/courserobot' target="_blank" rel="noopener noreferrer">CourseRobot</a>
       </Col>
       <Col md="4" >
         <Link className={"Footer-Logo"} to='/'>Course-O-Meter</Link>
